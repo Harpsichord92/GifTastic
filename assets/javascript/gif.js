4719ecff118bf92bf8d4ebee24332c26c31c7ba3
Ryan Lee <marble92@sbcglobal.net>
2017-12-04 12:56:00 -0600

    page populates with still images now, only need start/stop gifs to complete homework

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -1,13 +1,13 @@
-// Initial array of movies
+// Initial array of animals
       var topics = ["Dogs", "Cats", "Otters", "Red Pandas"];
 
-      // displayMovieInfo function re-renders the HTML to display the appropriate content
+      // displayGifInfo function re-renders the HTML to display the appropriate content
       function displayGifInfo() {
 
         var topics = $(this).attr("data-name");
         var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=zd12mHAtRAtU8VMjnD9Y5o2q26831qtq&q=" + topics + "&limit=10&offset=0";
 
-        // Creates AJAX call for the specific movie button being clicked
+        // Creates AJAX call for the specific animal button being clicked
         $.ajax({
           url: queryURL,
           method: "GET"
@@ -27,7 +27,7 @@
           //Retrieves the Images data
           // Creates an element to have the images displayed
           // Displays the images
-          $(animaldiv).append('<img src="' + response.data[i].url + '"/>')
+          $(animaldiv).append('<img src="' + response.data[i].images.fixed_width_still.url + '"/>')
           
           // Puts the entire animal gif above the previous animals.
           $('#animals').prepend(animaldiv)
@@ -41,28 +41,34 @@
       function renderButtons() {
 
         // Deletes the animal prior to adding new animals
-        // (this is necessary otherwise you will have repeat buttons)
         $("#animalButtons").empty();
+
         // Loops through the array of animals
         for (var i = 0; i < topics.length; i++) {
 
           // Then dynamicaly generates buttons for each animal in the array
           // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
           var a = $("<button>");
+
           // Adds a class of animals to our button
           a.addClass("animals");
+
           // Added a data-attribute
           a.attr("data-name", topics[i]);
+
           // Provided the initial button text
           a.text(topics[i]);
+
           // Added the button to the buttons-view div
           $("#animalButtons").append(a);
+
         }
       }
 
       // This function handles events where the add animal button is clicked
       $("#addAnimal").on("click", function(event) {
         event.preventDefault();
+
         // This line of code will grab the input from the textbox
         var topic = $("#animal-input").val().trim();
 
@@ -71,6 +77,7 @@
 
         // Calling renderButtons which handles the processing of our animal array
         renderButtons();
+        
       });
 
       // Adding click event listeners to all elements with a class of "animals"
